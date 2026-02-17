88b01c6f8c6f5d6e37c060c6648408830d532328
incepthink <incepthink@gmail.com>
2026-02-17 20:18:38 +0530

    cors

--- src/components/ConnectButton.tsx
+++ src/components/ConnectButton.tsx
@@ -1,13 +1,23 @@
 "use client";
 
+import type { CSSProperties } from "react";
 import { ConnectButton as RainbowConnectButton } from "@rainbow-me/rainbowkit";
 
-export default function ConnectButton() {
+interface ConnectButtonProps {
+  fullWidth?: boolean;
+  label?: string;
+  sx?: CSSProperties;
+  style?: CSSProperties;
+}
+
+export default function ConnectButton({ fullWidth, style }: ConnectButtonProps) {
   return (
-    <RainbowConnectButton
-      showBalance={false}
-      accountStatus="address"
-      chainStatus="icon"
-    />
+    <div style={{ width: fullWidth ? "100%" : undefined, ...style }}>
+      <RainbowConnectButton
+        showBalance={false}
+        accountStatus="address"
+        chainStatus="icon"
+      />
+    </div>
   );
 }
